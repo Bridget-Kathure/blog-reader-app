f22d945c437b30540bb0487a0442fe983cd30667
Bridget-Kathure <bridgetkathuretitus@gmail.com>
2024-07-02 08:35:24 +0300

    added files

---
+++ app/src/main/java/Blog.kt
@@ -0,0 +1,8 @@
+
+data class Article(
+    var name: String,
+    var publish:String,
+    var title:String,
+    var preview:String,
+    var link:String
+)
--- app/src/main/java/com/akirachix/blog_reader_app/MainActivity.kt
+++ app/src/main/java/com/akirachix/blog_reader_app/MainActivity.kt
@@ -1,20 +1,38 @@
-package com.akirachix.blog_reader_app
+package com.example.article
 
+import Article
+import BlogAdapter
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akirachix.blog_reader_app.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.rvRecyclerView.layoutManager=LinearLayoutManager(this)
+        displayArticles()
+
+    }
+    fun displayArticles(){
+        val article1=Article("Kathure","2023","The woods","The woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+        val article2=Article("Nabacwa","2022","The man","The woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+        val article3=Article("Nyaga","2021","Gaint","The woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+        val article4=Article("Bridget","2020","Hero","The woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+        val article5=Article("Karen","2012","Grow","The woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+        val article6=Article("Temko","2013","From","The woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+        val article7=Article("Joy","2013","Become","The woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+        val article8=Article("Pearl","2009","Accidental","TThe woods  were dark and hostile the night was long and hauntinng as she bravely traveled the wilderness","https://www.google.com/search")
+
+        val article= listOf(article1,article2,article3,article5,article6,article7,article8,article4)
+        val blogAdapter=BlogAdapter(article)
+        binding.rvRecyclerView.adapter=blogAdapter
     }
 }
